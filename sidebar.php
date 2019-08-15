6a62f9fc18250849640cf923875632552544e5f7
lkm543 <lkm543@gmail.com>
2019-08-15 11:48:04 +0800

    Bug fix

--- sidebar.php
+++ sidebar.php
@@ -10,22 +10,22 @@
     <li class='tab' data-tabid="HW1"><a href="#HW1">Homework 1</a></li>
 	<li class='tab' data-tabid="Chapter02"><a href="#Chapter02">Chapter 02 重載運算子、類別間的關係</a></li>
 	<li class='tab' data-tabid="Chapter03"><a href="#Chapter03">Chapter 03 類別的繼承</a></li>
-    <li class='tab' data-tabid="HW2"><a href="#HW1">Homework 2</a></li>
+    <li class='tab' data-tabid="HW2"><a href="#HW2">Homework 2</a></li>
 	<li class='tab' data-tabid="Chapter04"><a href="#Chapter04">Chapter 04 抽象與虛擬函式</a></li>
 	<li class='tab' data-tabid="Chapter05"><a href="#Chapter05">Chapter 05 函式模板與類別模板(Template)</a></li>
-    <li class='tab' data-tabid="HW3"><a href="#HW1">Homework 3</a></li>
+    <li class='tab' data-tabid="HW3"><a href="#HW3">Homework 3</a></li>
 	<li class='tab' data-tabid="Chapter06"><a href="#Chapter06">Chapter 06 類別的進階使用</a></li>
-    <li class='tab' data-tabid="HW4"><a href="#HW1">Homework 4</a></li>
+    <li class='tab' data-tabid="HW4"><a href="#HW4">Homework 4</a></li>
 	<li class='tab' data-tabid="Chapter07"><a href="#Chapter07">Chapter 07 命名空間(namespace)</a></li>
 	<li class='tab' data-tabid="Chapter08"><a href="#Chapter08">Chapter 08 動態與靜態連結</a></li>
-    <li class='tab' data-tabid="HW5"><a href="#HW1">Homework 5</a></li>
+    <li class='tab' data-tabid="HW5"><a href="#HW5">Homework 5</a></li>
 	<li class='tab' data-tabid="Chapter09"><a href="#Chapter09">Chapter 09 Process與Thread</a></li>
-    <li class='tab' data-tabid="HW6"><a href="#HW1">Homework 6</a></li>
+    <li class='tab' data-tabid="HW6"><a href="#HW6">Homework 6</a></li>
 	<li class='tab' data-tabid="Chapter10"><a href="#Chapter10">Chapter 10 C/C++的編譯</a></li>
-    <li class='tab' data-tabid="HW7"><a href="#HW1">Homework 7</a></li>
+    <li class='tab' data-tabid="HW7"><a href="#HW7">Homework 7</a></li>
 	<li class='tab' data-tabid="Chapter11"><a href="#Chapter11">Chapter 11 設計模式初探</a></li>
 	<li class='tab' data-tabid="Chapter12"><a href="#Chapter12">Chapter 12 實作─簡單工廠模式</a></li>
-    <li class='tab' data-tabid="HW8"><a href="#HW1">Homework 8</a></li>
+    <li class='tab' data-tabid="HW8"><a href="#HW8">Homework 8</a></li>
 	<li class='tab' data-tabid="Chapter13"><a href="#Chapter13">Chapter 13 實作─策略模式</a></li>
 	<li class='tab' data-tabid="Chapter14"><a href="#Chapter14">Chapter 14 實作─單例模式</a></li>
 </ul>
