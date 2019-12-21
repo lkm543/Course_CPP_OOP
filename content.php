32d6ab1076fe4c054b3294be3232a3b8b314105d
lkm543 <lkm543@gmail.com>
2019-12-21 17:50:03 +0800

    Update content.php

--- content.php
+++ content.php
@@ -11,6 +11,10 @@
 		require_once("Chapter0".$x.".php");
 		echo '</div>';
 	} 
+	echo '<div class="tabContent" data-id="Introduction" style="display:none">';
+	echo "<h3><a href=\"Lectures\\Introduction.pptx\">PPT下載連結</a></h3> ";
+	require_once("Introduction.php");
+	echo '</div>';
 	for ($x = 10; $x <= 14; $x++) {
     	echo '<div class="tabContent" data-id="Chapter'.$x.'" style="display:none">';
 		echo "<h3><a href=\"Lectures\\Chapter".$x.".pptx\">PPT下載連結</a></h3> ";
