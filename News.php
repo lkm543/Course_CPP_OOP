ea378c2057ffbe68cdfcf945a474ecb9eeb61a5f
lkm543 <lkm543@gmail.com>
2021-02-07 11:49:24 +0800

    Update News.php

--- News.php
+++ News.php
@@ -1,9 +1,18 @@
-<h2>這不是一門輕鬆的課程，若您同時有其他外務，修課前請三思</h2>
 
-<h2>無線網路Wifi帳號密碼(2019下半年)<br>
-SSID：CSIE_guest<br>
-帳號：guest_H7C8B<br>
-密碼：IM6DMBRR</h2>
+<h2>重要公告</h2>
+<ul>
+	<li>基礎班的<a href="https://www.lkm543.site/Course/Cpp/">連結</a>，帳號CPP、密碼ILOVECPP</li>
+	<li>進階班(資結演算法)的<a href="https://www.lkm543.site/Course/DS_AL/">連結</a>，帳號DSAL、密碼ILOVEDSAL</li>
+	<li>本門課的<a href="https://discord.gg/vY9NU53Mzn">Discord連結</a></li>
+	<li>不是本門課但可以參考的<a href="https://www.facebook.com/%E7%94%9F%E6%B4%BB%E4%B8%AD%E7%9A%84%E7%A8%8B%E5%BC%8F-108418124301138">粉絲專頁</a></li>
+	<li>無線網路Wifi帳號密碼(2021上半年)</li>
+	<ul>
+		<li>SSID：CSIE_guest</li>
+		<li>帳號：guest_LWV2V</li>
+		<li>密碼：QH1NL7QG</li>
+	</ul>
+</ul>
+
 
 <h2>2019/08/14</h2>
 <ul>
