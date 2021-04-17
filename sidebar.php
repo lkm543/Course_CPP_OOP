2b276f875d2126e1e6326b62a70079617e59b60d
lkm543 <lkm543@gmail.com>
2021-04-17 08:32:35 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -5,37 +5,23 @@
 
 <h2 class="star">課程目錄</h2>
 <ul class="sb_menu etabs">
-	<li class='tab' data-tabid="Chapter00"><a href="#Chapter00">Chapter 00 基本語法複習</a></li>
-	<li class='tab' data-tabid="Chapter01"><a href="#Chapter01">Chapter 01 結構(Structure)</a></li>
-    <li class='tab' data-tabid="HW1"><a href="#HW1">Homework 1</a></li>
+	<li class='tab' data-tabid="Chapter01"><a href="#Chapter01">Chapter 01 物件導向概論</a></li>
+	<li class='tab' data-tabid="Chapter02"><a href="#Chapter02">Chapter 02 類別的權限管理</a></li>
+	<li class='tab' data-tabid="Chapter03"><a href="#Chapter03">Chapter 03 類別間的繼承與複寫</a></li>
+	<li class='tab' data-tabid="Chapter04"><a href="#Chapter04">Chapter 04 抽象、虛擬與多型</a></li>
+	<li class='tab' data-tabid="Chapter05"><a href="#Chapter05">Chapter 05 函式模板與類別模板(Template)</a></li>
+	<li class='tab' data-tabid="Chapter06"><a href="#Chapter06">Chapter 06 類別補充</a></li>
+	<li class='tab' data-tabid="Chapter07"><a href="#Chapter07">Chapter 07 設計模式初探</a></li>
+	<li class='tab' data-tabid="Chapter08"><a href="#Chapter08">Chapter 08 設計模式─簡單工廠模式</a></li>
+	<li class='tab' data-tabid="Chapter09"><a href="#Chapter09">Chapter 09 設計模式─策略模式</a></li>
+	<li class='tab' data-tabid="Chapter10"><a href="#Chapter10">Chapter 10 設計模式─單例模式</a></li>
 
-	<li class='tab' data-tabid="Chapter02"><a href="#Chapter02">Chapter 02 物件的使用</a></li>
-	<li class='tab' data-tabid="Chapter03"><a href="#Chapter03">Chapter 03 重載運算子、類別間的關係</a></li>
-    <li class='tab' data-tabid="HW2"><a href="#HW2">Homework 2</a></li>
-
-	<li class='tab' data-tabid="Chapter04"><a href="#Chapter04">Chapter 04 類別的繼承</a></li>
-	<li class='tab' data-tabid="Chapter05"><a href="#Chapter05">Chapter 05 抽象與虛擬函式</a></li>
-    <li class='tab' data-tabid="HW3"><a href="#HW3">Homework 3</a></li>
-
-	<li class='tab' data-tabid="Chapter06"><a href="#Chapter06">Chapter 06 類別的進階使用</a></li>
-	<li class='tab' data-tabid="Chapter07"><a href="#Chapter07">Chapter 07 函式模板與類別模板(Template)</a></li>
-    <li class='tab' data-tabid="HW4"><a href="#HW4">Homework 4</a></li>
-
-	<li class='tab' data-tabid="Chapter08"><a href="#Chapter08">Chapter 08 命名空間(namespace)</a></li>
-	<li class='tab' data-tabid="Chapter09"><a href="#Chapter09">Chapter 09 動態連結函式庫</a></li>
-    <li class='tab' data-tabid="HW5"><a href="#HW5">Homework 5</a></li>
-
-	<li class='tab' data-tabid="Chapter10"><a href="#Chapter10">Chapter 10 物件導向與例外處理</a></li>
-	<li class='tab' data-tabid="Chapter11"><a href="#Chapter11">Chapter 11 標準模板庫(STL)</a></li>
-    <li class='tab' data-tabid="HW6"><a href="#HW6">Homework 6</a></li>
-
-	<li class='tab' data-tabid="Chapter12"><a href="#Chapter12">Chapter 12 C/C++的編譯過程</a></li>
-    <li class='tab' data-tabid="HW7"><a href="#HW7">Homework 7</a></li>
-
-	<li class='tab' data-tabid="Chapter13"><a href="#Chapter13">Chapter 13 設計模式初探</a></li>
-	<li class='tab' data-tabid="Chapter14"><a href="#Chapter14">Chapter 14 實作─簡單工廠模式</a></li>
-    <li class='tab' data-tabid="HW8"><a href="#HW8">Homework 8</a></li>
-
-	<li class='tab' data-tabid="Chapter15"><a href="#Chapter15">Chapter 15 實作─策略模式</a></li>
-	<li class='tab' data-tabid="Chapter16"><a href="#Chapter16">Chapter 16 實作─單例模式</a></li>
+	<li class='tab' data-tabid="Chapter11"><a href="#Chapter11">Chapter 11 Effective C++</a></li>
+	<li class='tab' data-tabid="Chapter12"><a href="#Chapter12">Chapter 12 各式資料型態</a></li>
+	<li class='tab' data-tabid="Chapter13"><a href="#Chapter13">Chapter 13 函式指標與智慧指標</a></li>
+	<li class='tab' data-tabid="Chapter14"><a href="#Chapter14">Chapter 14 物件導向與例外處理</a></li>
+	<li class='tab' data-tabid="Chapter15"><a href="#Chapter15">Chapter 15 Process 與 Thread 的使用</a></li>
+	<li class='tab' data-tabid="Chapter16"><a href="#Chapter16">Chapter 16 命名空間(namespace)</a></li>
+	<li class='tab' data-tabid="Chapter17"><a href="#Chapter17">Chapter 17 作業系統與程式編譯</a></li>
+	<li class='tab' data-tabid="Chapter18"><a href="#Chapter18">Chapter 18 手動編譯與 Makefile 撰寫</a></li>
 </ul>
